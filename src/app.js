3da626eef713ea5388f402fa3978025ab7adc0ad
Pedro H Mognon <phm1993@gmail.com>
2022-03-08 15:46:04 -0300

    Returning contract if user is client or contractor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require('sequelize');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
 
@@ -9,13 +10,21 @@ app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
   const { Contract } = req.app.get('models');
+  const userProfile = req.profile;
   const { id } = req.params;
-  const contract = await Contract.findOne({ where: { id } });
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [
+        { ContractorId: userProfile.id },
+        { ClientId: userProfile.id },
+      ],
+    },
+  });
   if (!contract) return res.status(404).end();
   res.json(contract);
 });
